f6308b7a3e35a35481e6d95cfacee35a344860cf
sunpeifu <sunpeifu@aliyun.com>
2026-03-17 16:44:58 +0800

    for循环使用enumerate 获取index和指定start index

--- Doc_Chunk/langchain_character_text_split.py
+++ Doc_Chunk/langchain_character_text_split.py
@@ -12,8 +12,11 @@ characterTextSpliter = CharacterTextSplitter(
     chunk_size = 100, # chunk_size 100 是按照多少个字符切分(如果是中文)
     chunk_overlap = 10 # chunk_overlap 10 对应中文是多少
 )
+
+# enumerate是for中 同时拿到index和具体element的方法 , 设置start指定index开始位置
 split_documents = characterTextSpliter.split_documents(docs)
-for split_doc in split_documents:
+for i,split_doc in enumerate(split_documents, start=1):
+    print("当前的index:",i)
     print("切分后的metadata:", split_doc.metadata)
     print("切分后的pagecontext:", split_doc.page_content)
 
